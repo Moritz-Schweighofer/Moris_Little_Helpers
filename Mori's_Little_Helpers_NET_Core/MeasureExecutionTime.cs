19d1e3e4e0a623926d17d85cc4efb3e7622a0a73
Schweighofer Moritz <moritz.schweighofer@dmgmori.com>
2020-05-26 14:42:04 +0200

    Added Time Measurment

---
+++ Mori's_Little_Helpers_NET_Core/MeasureExecutionTime.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Schweigm_NETCore_Helpers
+{
+    public static class MeasureExecutionTime
+    {
+        public static long  MeasureSinge(Action action)
+        {
+            var stopwatch = Stopwatch.StartNew();
+            action();
+            var elapsedTime = stopwatch.ElapsedMilliseconds;
+            stopwatch.Reset();
+
+            return elapsedTime;
+        }
+    }
+}

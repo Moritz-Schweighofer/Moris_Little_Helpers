e5d848e5e5cdc626626ada31ad8f443758d77d70
Schweighofer Moritz <moritz.schweighofer@dmgmori.com>
2020-05-27 09:38:59 +0200

    Added Encoding & Enum Description

---
+++ Mori's_Little_Helpers_NET_Core/EncodeBase64.cs
@@ -0,0 +1,49 @@
+﻿// ===============================
+//
+// COMPANY:         DMG MORI Software Solutions
+// AUTHOR:          Moritz Schweighofer, Software Developer Control Systems Integrations
+// CREATE DATE:     05-11-2019
+//
+// ===============================
+//
+// SVN-REVISION:    $Revision:  $
+// SVN-AUTHOR:      $Author:  $
+// SVN-DATE:        $Date:  $
+//
+// ===============================
+//
+// PURPOSE:         Class to Encode a Username and Password in Base64
+// SPECIAL NOTES:
+//
+// ===============================
+//
+// Change History:
+//
+//==================================
+
+
+using System;
+using System.Text;
+
+namespace Schweigm_NETCore_Helpers
+{
+    public class EncodeUsernamePasswordBase64
+    {
+
+        /// <summary>
+        /// This static Function Encodes the Username and Password so the Modules can read it
+        /// </summary>
+        public static string Encode(string username, string password)
+        {
+            try
+            {
+                var encoded = Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(username + ":" + password)); //don't ask
+                return encoded;
+            }
+            catch
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ Mori's_Little_Helpers_NET_Core/GetEnumDescription.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Schweigm_NETCore_Helpers
+{
+    public static class EnumDescription
+    {
+        // Return the description of enum value
+        public static string GetEnumDescription(Enum value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            var attributes = (DescriptionAttribute[])fieldInfo?.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return attributes != null && attributes.Length > 0 ? attributes[0].Description : value.ToString();
+
+            // Return the value of the attribute
+        }
+    }
+}
